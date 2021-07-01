772ebd6a7f7f72192a4b8b0ec48644a012e2261d
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-07-01 02:47:27 +0200

    GET PUT POST DEL and file upload added to a separate route

--- src/server.js
+++ src/server.js
@@ -2,17 +2,34 @@ import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 import blogRouter from "./blogs/index.js";
-// import {join, dirname} from "path";
-// import {fileURLToPath} from "url";
+import {join, dirname} from "path";
+import {fileURLToPath} from "url";
+import {
+  catchErrorMiddleware,
+  badRequestMiddleware,
+  notFoundMiddleware,
+} from "./errorMiddlewares.js";
+import filesRouter from "./services/files/index.js";
+import postRouter from "./blogPosts/index.js";
 
+// Path for the file systems;
+const publicFolderPath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "../public"
+);
+
+// Initiate and Server details;
 const server = express();
 const port = process.env.PORT || 3001;
 
-// section for routes and global middlewares
+// section for routes and global middlewares;
+
+//Cross Platform Origin verification;
 const whitelist = [
   process.env.FRONTEND_URL,
   process.env.FRONTEND_PROD_URL,
   "http://localhost:3001",
+  "http://localhost:3000",
 ];
 server.use(
   cors({
@@ -33,6 +50,13 @@ server.use(express.json());
 // Server Routes
 
 server.use("/blogs", blogRouter);
+server.use("/files", filesRouter);
+server.use("/posts", postRouter);
+
+//Section for the error middlewares
+server.use(notFoundMiddleware);
+server.use(badRequestMiddleware);
+server.use(catchErrorMiddleware);
 
 // Listen the server at port 3001;
 console.table(listEndpoints(server));
